12dba16daa097e2b84b64662895e24cfde25b558
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-03 15:26:26 +0200

    initial import of SPRING INITIALIZR result

---
+++ src/main/java/edu/iis/mto/Lab52Application.java
@@ -0,0 +1,12 @@
+package edu.iis.mto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab52Application.class, args);
+	}
+}
---
+++ src/test/java/edu/iis/mto/Lab52ApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.iis.mto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lab52ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
