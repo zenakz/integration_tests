45033291e31312aab1c31528a322ae9e16aca105
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-04 20:36:15 +0200

    logging errors

--- blogapi/src/main/java/edu/iis/mto/blog/api/exceptions/ErrorHandling.java
+++ blogapi/src/main/java/edu/iis/mto/blog/api/exceptions/ErrorHandling.java
@@ -1,5 +1,7 @@
 package edu.iis.mto.blog.api.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -9,7 +11,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ErrorHandling {
 
+    private final static Logger logger = LoggerFactory.getLogger(ErrorHandling.class);
+
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(code = HttpStatus.CONFLICT)
-    public void dataIntegrityException() {}
+    public void dataIntegrityException(DataIntegrityViolationException exc) {
+        logger.error(exc.getMessage());
+    }
 }
