613359112be72b9e1d28d42e519dac1b27dfcb43
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-04 12:48:50 +0200

    test main class added

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
@@ -0,0 +1,32 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.junit.BeforeClass;
+
+import com.jayway.restassured.RestAssured;
+
+public class FunctionalTests {
+
+    @BeforeClass
+    public static void setup() {
+        String port = System.getProperty("server.port");
+        if (port == null) {
+            RestAssured.port = Integer.valueOf(8080);
+        } else {
+            RestAssured.port = Integer.valueOf(port);
+        }
+
+        String basePath = System.getProperty("server.base");
+        if (basePath == null) {
+            basePath = "";
+        }
+        RestAssured.basePath = basePath;
+
+        String baseHost = System.getProperty("server.host");
+        if (baseHost == null) {
+            baseHost = "http://localhost";
+        }
+        RestAssured.baseURI = baseHost;
+
+    }
+
+}
