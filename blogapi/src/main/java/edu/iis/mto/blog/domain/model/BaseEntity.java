6acedfaafb21c00a9a2221a80a576415040e3a96
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-13 17:06:56 +0200

    id as Long

--- blogapi/src/main/java/edu/iis/mto/blog/domain/model/BaseEntity.java
+++ blogapi/src/main/java/edu/iis/mto/blog/domain/model/BaseEntity.java
@@ -14,13 +14,13 @@ public class BaseEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
