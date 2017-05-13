4ffa505f7f31d9882a5077bd4e1fd89ccfd710e6
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-13 17:38:39 +0200

    Id class added

---
+++ blogapi/src/main/java/edu/iis/mto/blog/dto/Id.java
@@ -0,0 +1,18 @@
+package edu.iis.mto.blog.dto;
+
+public class Id {
+
+    private Long id;
+
+    public Id(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
