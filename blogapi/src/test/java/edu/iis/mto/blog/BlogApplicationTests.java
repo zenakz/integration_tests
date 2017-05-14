926daf3286f262103f4b57d3dadfb166f83273a9
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-14 10:15:03 +0200

    fixing configuration problem

--- blogapi/src/test/java/edu/iis/mto/blog/BlogApplicationTests.java
+++ blogapi/src/test/java/edu/iis/mto/blog/BlogApplicationTests.java
@@ -3,14 +3,15 @@ package edu.iis.mto.blog;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ComponentScan("edu.iis.mto.blog.domain")
 public class BlogApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {}
 
 }
