c6e2a71d2745d9ab95d9b769c154a9091517e8f4
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-14 14:35:15 +0200

    domain layer sample test added

---
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -0,0 +1,43 @@
+package edu.iis.mto.blog.domain;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import edu.iis.mto.blog.api.request.UserRequest;
+import edu.iis.mto.blog.domain.model.AccountStatus;
+import edu.iis.mto.blog.domain.model.User;
+import edu.iis.mto.blog.domain.repository.UserRepository;
+import edu.iis.mto.blog.mapper.DataMapper;
+import edu.iis.mto.blog.services.BlogService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogManagerTest {
+
+    @MockBean
+    UserRepository userRepository;
+
+    @Autowired
+    DataMapper dataMapper;
+
+    @Autowired
+    BlogService blogService;
+
+    @Test
+    public void creatingNewUserShouldSetAccountStatusToNEW() {
+        blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
+        ArgumentCaptor<User> userParam = ArgumentCaptor.forClass(User.class);
+        Mockito.verify(userRepository).save(userParam.capture());
+        User user = userParam.getValue();
+        Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+}
