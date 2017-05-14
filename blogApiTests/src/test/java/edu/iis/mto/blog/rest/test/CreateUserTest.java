21d8df7fbefb7014ee07efbff4233c69ac22fd8e
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-14 09:03:43 +0200

    functional test added

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -0,0 +1,19 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+public class CreateUserTest extends FunctionalTests {
+
+    @Test
+    public void postFormWithMalformedRequestDataReturnsBadRequest() {
+        JSONObject jsonObj = new JSONObject().put("email", "tracy@domain.com");
+        RestAssured.given().accept(ContentType.JSON).header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString()).expect().log().all().statusCode(HttpStatus.SC_CREATED).when()
+                .post("/blog/user");
+    }
+}
