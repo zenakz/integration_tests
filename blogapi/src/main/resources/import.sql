61c35617345ec7eff7880559fd6dfe5f60881b30
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2017-05-14 14:07:04 +0200

    comments added

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,1 +1,3 @@
+--this script initiates db for h2 db (used in test profile)
 insert into user (id, account_status, email, first_name, last_name) values (null, 'CONFIRMED', 'john@domain.com', 'John', 'Steward')
+insert into user (id, account_status, email, first_name) values (null, 'NEW', 'brian@domain.com', 'Brian')
