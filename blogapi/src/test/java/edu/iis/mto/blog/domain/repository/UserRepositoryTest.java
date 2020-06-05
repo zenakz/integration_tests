7f7a48c89db7643ae1a7feacfd17674eaea522c4
Zenakz <Zenakz@github.com>
2020-06-05 14:35:30 +0200

    3.1.2

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -19,53 +19,70 @@ import org.springframework.test.context.junit4.SpringRunner;
 import edu.iis.mto.blog.domain.model.AccountStatus;
 import edu.iis.mto.blog.domain.model.User;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
-public class UserRepositoryTest {
+@RunWith(SpringRunner.class) @DataJpaTest public class UserRepositoryTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
+    @Autowired private TestEntityManager entityManager;
 
-    @Autowired
-    private UserRepository repository;
+    @Autowired private UserRepository repository;
 
     private User user;
 
-    @Before
-    public void setUp() {
-        user = createUser("Jan", "","john@domain.com");
+    @Before public void setUp() {
+        user = createUser("Jan", "", "john@domain.com");
         repository.deleteAll();
     }
 
-    @Test
-    public void shouldFindNoUsersIfRepositoryIsEmpty() {
+    @Test public void shouldFindNoUsersIfRepositoryIsEmpty() {
 
         List<User> users = repository.findAll();
 
         assertThat(users, hasSize(0));
     }
 
-    @Test
-    public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
+    @Test public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
         User persistedUser = entityManager.persist(user);
         List<User> users = repository.findAll();
 
         assertThat(users, hasSize(1));
-        assertThat(users.get(0)
-                        .getEmail(),
-                equalTo(user.getEmail()));
+        assertThat(users.get(0).getEmail(), equalTo(user.getEmail()));
     }
 
-    @Test
-    public void shouldStoreANewUser() {
+    @Test public void shouldStoreANewUser() {
 
         User persistedUser = repository.save(user);
 
         assertThat(persistedUser.getId(), notNullValue());
     }
 
+    @Test public void shouldFindAllUsersWithSameName() {
+        repository.save(createUser("Jan", "", "mail@gmail.com"));
+        repository.save(createUser("Jan", "", "mail2@gmail.com"));
+        repository.save(createUser("Marcin", "Adamski", "user3@gmail.com"));
+        List<User> list = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("Jan", "NameNotInRepo",
+                "MailNotInRepo");
+        assertThat(list, hasSize(2));
+        assertThat(list.get(0).getFirstName(), equalTo("Jan"));
+    }
 
-    public User createUser(String name, String lastName, String email){
+    @Test public void shouldFindAllGmailUsers() {
+        repository.save(createUser("Jan", "Kowalski", "user1@gmail.com"));
+        repository.save(createUser("Stefan", "Nowak", "user2@gmail.com"));
+        repository.save(createUser("Marcin", "Adamski", "user3@gmail.com"));
+        List<User> list = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("NameNotInRepo",
+                "NotInRepo", "gmail");
+        assertThat(list, hasSize(3));
+    }
+
+    @Test public void shouldFindNoUsers() {
+        repository.save(createUser("Jan", "Kowalski", "user1@gmail.com"));
+        repository.save(createUser("Stefan", "Nowak", "user2@gmail.com"));
+        repository.save(createUser("Marcin", "Adamski", "user3@gmail.com"));
+        List<User> list = repository.findByFirstNameContainingOrLastNameContainingOrEmailContainingAllIgnoreCase("NameNotInRepo",
+                "NameNotInRepo", "MailNotInRepo");
+        assertThat(list, hasSize(0));
+    }
+
+    public User createUser(String name, String lastName, String email) {
         user = new User();
         user.setFirstName(name);
         user.setLastName(lastName);
